3a3ed60d054dab9aae630c14ab6d5fd01a23597f
Marta Jovanovic <jovanovic99marta@gmail.com>
2022-04-29 16:55:53 +0200

    .

--- app/src/main/java/com/example/koktelmajstor/MainActivity.java
+++ app/src/main/java/com/example/koktelmajstor/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.koktelmajstor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void prelazakNaRecp(View view) {
+
+    }
+
+    public void prelazakNaOmilj(View view) {
+    }
+
+    public void prelazakNaIgr(View view) {
     }
 }
